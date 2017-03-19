9170d8e03d3db4c7ba5f54b8865a2824e902fe3e
sebastianbakala <cubisticshock@gmail.com>
2017-03-19 01:38:57 +0100

    Add files via upload

---
+++ lib/headers/generuj.h
@@ -0,0 +1,7 @@
+#ifndef generuj_h
+#define generuj_h
+
+void gen_rand(int tab[], int rozmiar, int minimum, int maximum, int powtorzenia);
+void gen_print(int tab[],int rozmiar);
+
+#endif // generuj_h
