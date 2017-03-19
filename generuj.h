9560339be99ac3cbdba87bf0b19a64f788051cc5
sebastianbakala <cubisticshock@gmail.com>
2017-03-19 01:39:20 +0100

    Delete generuj.h

--- generuj.h
+++
@@ -1,7 +0,0 @@
-#ifndef generuj_h
-#define generuj_h
-
-void gen_rand(int tab[], int rozmiar, int minimum, int maximum, int powtorzenia);
-void gen_print(int tab[],int rozmiar);
-
-#endif // generuj_h
